6c6b99f7ead09f82890d2551a96a987ecc253757
Raysmond <jiankunlei@126.com>
2016-05-12 08:26:27 +0800

    U

--- lab-docker/build_images.sh
+++ lab-docker/build_images.sh
@@ -6,5 +6,5 @@ unset DOCKER_TLS_VERIFY
 
 for (( i = 0; i < 10; i++ )); do
     IP="10.141.201.20$i"
-    docker -H tcp://$IP:2375 build -t raysmond/artirest:0.1.2 build/docker
+    docker -H tcp://$IP:2375 build -t raysmond/artirest:0.1.3 build/docker
 done
